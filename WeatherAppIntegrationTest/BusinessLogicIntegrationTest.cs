475a845a8b808625576896c747b398b58770701f
anvarMajidov <majidovanvar30.11.2002@gmail.com>
2022-05-05 21:16:36 +0500

    Add integration test to BL

---
+++ WeatherAppIntegrationTest/BusinessLogicIntegrationTest.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic.models;
+using BusinessLogic.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WeatherAppIntegrationTest
+{
+    public class BusinessLogicIntegrationTest
+    {
+        [Theory]
+        [InlineData("London")]
+        [InlineData("Paris")]
+        [InlineData("Tokyo")]
+        public async Task WeatherService_ExistingCity_ReturnsWeatherModel(string city)
+        {
+            //arrange
+            WeatherService _service = new WeatherService();
+            
+            //act
+            Weather response = await _service.GetWeatherInfo(city);
+            double temperature = response.Main.Temp;
+
+            //assert
+            //regex matches all double and numeric values
+            Assert.Matches(@"-?\d+(?:\.\d+)?", temperature.ToString());
+        }
+    }
+}
