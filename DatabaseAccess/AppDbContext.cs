de09c7c67cc34925101be37e569dc04f5c17ea49
anvarMajidov <majidovanvar30.11.2002@gmail.com>
2022-05-17 11:00:30 +0500

    Move appDbContext to Data Layer

---
+++ DatabaseAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WeatherAPI.models;
+
+namespace DatabaseAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public AppDbContext() { }
+
+        public DbSet<WebWeather> WebWeathers { get; set; }
+        public DbSet<WebDailyTemp> WebDailyTemps { get; set; }
+        public DbSet<WebWeatherForecast> WeatherForecasts { get; set; }
+    }
+}
