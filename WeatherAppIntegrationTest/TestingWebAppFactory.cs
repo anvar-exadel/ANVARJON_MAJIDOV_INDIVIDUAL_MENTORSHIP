1035eaf597060eb76204aebe86672ebd4ded2198
anvarMajidov <majidovanvar30.11.2002@gmail.com>
2022-05-19 16:40:01 +0500

    Add not implemented integration test, and some .vs files

---
+++ WeatherAppIntegrationTest/TestingWebAppFactory.cs
@@ -0,0 +1,68 @@
+﻿using DatabaseAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAPI;
+
+namespace WeatherAppIntegrationTest
+{
+    // public class TestingWebAppFactory<T> : WebApplicationFactory<Startup>
+    // {
+    //     protected override void ConfigureWebHost(IWebHostBuilder builder)
+    //     {
+    //         builder.ConfigureServices(services =>
+    //         {
+    //             var dbContext = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+    //             if (dbContext != null)
+    //                 services.Remove(dbContext);
+
+    //             var config = new ConfigurationBuilder()
+    //                 .AddInMemoryCollection(cities)
+    //                 .Build();
+
+    //             var serviceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+    //             services.AddDbContext<AppDbContext>(options =>
+    //             {
+    //                 options.UseInMemoryDatabase("InMemoryEmployeeTest");
+    //                 options.UseInternalServiceProvider(serviceProvider);
+    //             });
+    //             //add iconfiguration services
+    //             services.AddSingleton<IConfiguration>(config);
+
+    //             var sp = services.BuildServiceProvider();
+
+    //             using (var scope = sp.CreateScope())
+    //             {
+    //                 using (var appContext = scope.ServiceProvider.GetRequiredService<AppDbContext>())
+    //                 {
+    //                     try
+    //                     {
+    //                         appContext.Database.EnsureCreated();
+    //                     }
+    //                     catch (Exception)
+    //                     {
+    //                         //Log errors
+    //                         throw;
+    //                     }
+    //                 }
+    //             }
+    //         });
+    //     }
+
+    //     public Dictionary<string, string> cities = new Dictionary<string, string>
+    //     {
+    //         { "WebCities:london", "486400" },
+    //         { "WebCities:paris", "60400" },
+    //         { "WebCities:tashkent", "1240000" },
+    //         { "WebCities:minsk", "3200" },
+    //         { "WebCities:canberra", "400" }
+    //     };
+    // }
+}
---
+++ WeatherAppIntegrationTest/WeatherControllerApiTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace WeatherAppIntegrationTest
+{
+    public class WeatherControllerApiTests
+    {
+        private readonly HttpClient _client;
+        public WeatherControllerApiTests(HttpClient client)
+        {
+            _client = client;
+        }
+    }
+}
