4c446f5121301f7e7cfc4546e8cf12b317b20f37
anvarMajidov <majidovanvar30.11.2002@gmail.com>
2022-05-30 17:39:48 +0500

    create service for sending message through rabbitMq

---
+++ BusinessLogic/services/SendRabbit.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLogic.interfaces;
+using RabbitMQ.Client;
+using Shared.apiResponse.mailResponse;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace BusinessLogic.services
+{
+    public class SendRabbit : IDisposable, ISendRabbit
+    {
+        private IConnection _connection;
+        private IModel _channel;
+
+        public SendRabbit()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void SendMessage(string message,string queue)
+        {
+            _channel.QueueDeclare(queue: queue,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(exchange: "",
+                                 routingKey: queue,
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        public void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}
