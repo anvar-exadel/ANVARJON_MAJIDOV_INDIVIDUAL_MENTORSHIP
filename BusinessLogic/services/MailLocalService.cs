00d59ccbdcc22eebf267531adf7822e0b3bda3a5
anvarMajidov <majidovanvar30.11.2002@gmail.com>
2022-05-31 15:42:32 +0500

    add flag for sending email locally or in separate project

---
+++ BusinessLogic/services/MailLocalService.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLogic.interfaces;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using Shared.apiResponse.mailResponse;
+using Shared.configFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.services
+{
+    public class MailLocalService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+
+        public MailLocalService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public void SendEmail(MailRequest mailer)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail));
+            message.To.Add(new MailboxAddress(mailer.ToEmail, mailer.ToEmail));
+            message.Subject = mailer.Subject;
+
+            message.Body = new TextPart("plain") { Text = mailer.Body };
+            try
+            {
+                using SmtpClient client = new SmtpClient();
+                client.Connect(_mailSettings.Host, _mailSettings.Port, true);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
